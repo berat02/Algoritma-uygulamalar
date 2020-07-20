a75927e778eab4b1bcb71b649257e2f3f0656589
beratyildirim0234@gmail.com <beratyildirim0234@gmail.com>
2020-07-20 17:41:20 +0300

    1.Buyuk Olan Sayıyı Bulma Örneği

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function buyuk_olanı_bul(){
+function buyuk_olani_bul() {
     var sayi1 = Number(document.getElementById('sayi1').value);
     var sayi2 = Number(document.getElementById('sayi2').value);
     var sonuc = document.getElementById('sonuc');
 
-    if(sayi1 >sayi2){
-        document.getElementById('sonuc').value ="Sayı1 :) "+sayi1+" Daha Büyük";
-    }else if(sayi1<sayi2){
-        document.getElementById('sonuc').value ="Sayı2 :) "+sayi2+" Daha Büyük";
-    }else{
-        document.getElementById('sonuc').value ="Eşit";
+    if (sayi1 > sayi2) {
+        sonuc.value = ("Sayı1 :) " + sayi1 + " Daha Büyük");
+    } else if (sayi1 < sayi2) {
+        sonuc.value = ("Sayı2 :) " + sayi2 + " Daha Büyük");
+    } else {
+        sonuc.value = ("Eşit");
     }
-
 }
