36debdd05a5fdd63a57aabc31757f4e2173fa441
Berat Yıldırım <beratyildirim0234@gmail.com>
2020-07-22 10:11:53 +0300

    2.Vize Final Hesaplama Örneği

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function buyuk_olani_bul() {
         sonuc.value = ("Eşit");
     }
 }
+function vize_final_hesapla(){
+    var vize = Number(document.getElementById('vize').value);
+    var final = Number(document.getElementById('final').value);
+    var sonuc = document.getElementById('sonuc');
+    var ortalama = vize * 0.40 + final * 0.60;
+    if(ortalama  >= 60){
+        sonuc.value = ("Ort: "+ortalama+" Öğrenci Geçti");
+        sonuc.style.color = "green";
+    }else{
+        sonuc.value = ("Ort: "+ortalama+" Öğrenci Geçemedi");
+        sonuc.style.color = "red";
+    }
+}
